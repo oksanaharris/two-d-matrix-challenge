8ea4fedb5349e3cd71e9a48b376e3cdfbe59fb55
Oksana Harr <oksana.harris@gmail.com>
2017-06-19 11:53:57 -1000

    updated parameter names to be more accurate

--- matrix.js
+++ matrix.js
@@ -1,12 +1,12 @@
 
 /*jshint esversion: 6 */
 
-var createMatrix = (function (col, row, fill){
+var createMatrix = (function (row, col, fill){
   var arr = [];
 
-  for (var i = 0; i < col; i++){
+  for (var i = 0; i < row; i++){
     var innerarr = [];
-    for (var j = 0; j < row; j++){
+    for (var j = 0; j < col; j++){
       innerarr.push(fill);
     }
     arr.push(innerarr);
@@ -14,26 +14,26 @@ var createMatrix = (function (col, row, fill){
   return arr;
 })(2, 3, "old");
 
-function augmentMatrix (arr, col, row, fill) {
+function augmentMatrix (arr, row, col, fill) {
   
   var outLength = arr.length;
   var initialLength = arr[0].length;
   var innerarr;
   var inLength;
 
-  if (col < outLength || row < initialLength){
+  if (col < outLength || col < initialLength){
     throw new Error ('That ish dont fly.');
   } else {
     for (let i = 0; i < arr.length; i++){
               innerarr = arr[i];
         inLength = arr[i].length;
-          for (let j = 0; j < (row-inLength); j++ ){
+          for (let j = 0; j < (col-inLength); j++ ){
            innerarr.push(fill);
           }
-    for (let k = arr.length; k < col; k++){
+    for (let k = arr.length; k < row; k++){
         innerarr = [];
         inLength = 0;
-          for (let l = 0; l < (row-inLength); l++ ){
+          for (let l = 0; l < (col-inLength); l++ ){
             innerarr.push(fill);
           }
         arr.push(innerarr);
